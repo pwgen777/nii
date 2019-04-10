5d239c361a734f9d4ea9586bec00a34ef1e45332
pwgen777 <andrey.cherepanov1@mail.ru>
2019-04-11 01:28:45 +0300

    croll+hidden

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+    function replaceVerticalScrollByHorizontal(event) {
+    	if (event.deltaY != 0) {
+      	
+        window.scroll(window.scrollX + event.deltaY * 1, window.scrollY);
+        
+        
+      	event.preventDefault();
+      }
+      return;
+    }
+
+    
+    window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
