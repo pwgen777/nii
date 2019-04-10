436def0f2f16c899cd49d2b8dc7fb7fb7fdde446
pwgen777 <andrey.cherepanov1@mail.ru>
2019-04-11 02:40:20 +0300

    commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 'use strict'
 
-    function replaceVerticalScrollByHorizontal(event) {
+function replaceVerticalScrollByHorizontal(event) {
     	if (event.deltaY != 0) {
-      	
         window.scroll(window.scrollX + event.deltaY * 1, window.scrollY);
-        
-        
       	event.preventDefault();
-      }
+        }
       return;
-    }
+}
+ window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
+
+
+
+
 
     
-    window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
