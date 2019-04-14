c000cd77e3e93c0192845d70da2525bc6790f5ff
pwgen777 <andrey.cherepanov1@mail.ru>
2019-04-15 01:39:40 +0300

    lot

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// горизонтальный скролл
 function replaceVerticalScrollByHorizontal(event) {
     	if (event.deltaY != 0) {
         window.scroll(window.scrollX + event.deltaY * 1, window.scrollY);
@@ -9,13 +9,13 @@ function replaceVerticalScrollByHorizontal(event) {
  window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
  
 
-
+// размеры области 
 var width , height;
 var getSize = document.getElementById('photo');
 getSize.onload = function() {
     width = this.offsetWidth;
     height = this.offsetHeight; 
-    var changeSize = document.getElementById("body-size"); 
+    var changeSize = document.getElementById("body"); 
         changeSize.style.width = (width+("px"));
         changeSize.style.height = (height+("px"));
     var chack = document.getElementById('contain');
@@ -23,6 +23,7 @@ getSize.onload = function() {
         chack.style.height = (height+("px"));
 }
 
+// скрол при зажатой ЛКМ
 
 document.getElementById('photo').ondragstart = function() { return false; };
 var x , y;
@@ -48,4 +49,23 @@ function mousemove(e) {
  }
 
  }
-  
+
+ //создание grid ячеек 
+
+  var dridEx = document.getElementById("gridEx");
+
+for (var i = 1; i < 22; i++) {
+ var newDiv = document.createElement('div');
+    newDiv.id=('div'+i);
+    gridEx.appendChild(newDiv);
+};
+
+for (var i = 1; i < 22; i++) {
+  var txt = document.getElementById('div'+i);
+  // txt.style.textAlign = center;
+  txt.innerText = ('Персональный id = '+i);
+ 
+ };
+ 
+
+
