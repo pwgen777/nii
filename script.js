ec4a15d2d40be9b54b36cae6ffee827472b8177c
pwgen777 <andrey.cherepanov1@mail.ru>
2019-04-14 22:10:15 +0300

    update scroll

--- script.js
+++ script.js
@@ -8,9 +8,43 @@ function replaceVerticalScrollByHorizontal(event) {
       return;
 }
  window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
+ 
 
 
+var width , height;
+var getSize = document.getElementById('foto');
+getSize.onload = function() {
+    width = this.offsetWidth;
+    height = this.offsetHeight; 
+    var changeSize = document.getElementById("html"); 
+        changeSize.style.width = (width+("px"));
+        changeSize.style.height = (height+("px"));
+    var chack = document.getElementById('contain');
+        chack.style.width = (width+("px"));
+        chack.style.height = (height+("px"));
+}
 
 
+document.getElementById('foto').ondragstart = function() { return false; };
+var x , y;
+document.onmousemove = mousemove;
+document.onmousedown = function(e) {
 
-    
+  e = e || window.event;
+   x = e.clientX;
+   y = e.clientY;
+}
+
+function mousemove(e) {
+
+ if (event.which == 1) 
+ {
+     if (x && y) {
+    window.scrollBy(x - e.clientX, y + e.clientY);
+  }
+  x = e.clientX;
+  y = e.clientY;
+ }
+
+ }
+  
